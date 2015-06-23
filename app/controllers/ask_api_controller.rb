27460340c846b95f10f06d364628a8178c91671f
Adrián Pérez <adrian@simbiotica.es>
2015-06-23 18:15:34 +0200

    Added examples for: imazon_use and imazon_wdpa

--- app/controllers/ask_api_controller.rb
+++ app/controllers/ask_api_controller.rb
@@ -1,7 +1,9 @@
 class AskApiController < ApplicationController
 	require "gfw_api_client"
 	def index
-		@imazon_iso = GfwApiClient.find_set('imazon-alerts','BRA')
-		@umd_iso 	= GfwApiClient.find_set('umd-loss-gain','BRA')
+		@imazon_iso  = GfwApiClient.find_set('imazon-alerts','iso','BRA')
+		@imazon_wdpa = GfwApiClient.find_set('imazon-alerts','wdpa','223')
+		@imazon_use  = GfwApiClient.find_set('imazon-alerts','use','{"type":"mining","id":9417}')
+		@umd_iso 	 = GfwApiClient.find_set('umd-loss-gain','iso','BRA')
 	end
 end
