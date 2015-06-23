66c3701f4b1980caf09da2102d4ca70e81fa0f92
Adrián Pérez <adrian@simbiotica.es>
2015-06-23 17:02:55 +0200

    added umd iso

--- app/controllers/ask_api_controller.rb
+++ app/controllers/ask_api_controller.rb
@@ -2,5 +2,6 @@ class AskApiController < ApplicationController
 	require "gfw_api_client"
 	def index
 		@imazon_iso = GfwApiClient.find_set('imazon-alerts','BRA')
+		@umd_iso 	= GfwApiClient.find_set('umd-loss-gain','BRA')
 	end
 end
